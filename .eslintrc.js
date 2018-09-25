47abdbb79b6e939c8cb947367253124184e7b007
yravinderkumar33 <yravinderkumar33@gmail.com>
2018-09-25 19:30:33 +0530

    2

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "import/prefer-default-export": "off",
+        "react/destructuring-assignment": ["enabled", 'never'],
+        "react/forbid-prop-types": "off",
+        "react/require-default-props": "off",
+        "react/no-array-index-key" : "off",
+             
+    }
+};
+
